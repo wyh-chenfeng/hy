b23e73e744836dbe85b168f216ac14b65a916d42
xiaolyuh <xiaolyuh@163.com>
2016-07-07 14:02:09 +0800

    初始化项目

---
+++ hy-domain/src/main/java/com/chenfeng/hy/domain/common/config/SystemConfig.java
@@ -0,0 +1,8 @@
+package com.chenfeng.hy.domain.common.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SystemConfig {
+
+}
---
+++ hy-site/src/main/java/com/chenfeng/hy/controller/App.java
@@ -0,0 +1,13 @@
+package com.chenfeng.hy.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
