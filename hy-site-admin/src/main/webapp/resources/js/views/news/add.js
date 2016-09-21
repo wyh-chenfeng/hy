ed54f0e72cc29a7aca6d4d0f5ed41df2ec7da53f
xiaolyuh <xiaolyuh@163.com>
2016-09-21 20:23:57 +0800

    富文本编辑器最终版

--- hy-site-admin/src/main/webapp/resources/js/views/news/add.js
+++ hy-site-admin/src/main/webapp/resources/js/views/news/add.js
@@ -81,6 +81,19 @@
 		initEditor: function() {
 			var options = {
 					serverUrl: 'initUpload',
+					toolbars: [[
+					            'fullscreen', 'source', '|', 'undo', 'redo', '|',
+					            'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'cleardoc', '|',
+					            'rowspacingtop', 'rowspacingbottom', 'lineheight', '|',
+					            'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|',
+					            'directionalityltr', 'directionalityrtl', 'indent', '|',
+					            'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|', 'touppercase', 'tolowercase', '|',
+					            'link', 'unlink', 'anchor', '|', 'imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
+					            'simpleupload', 'background', '|',
+					            'horizontal', 'date', 'time', 'spechars', '|',
+					            'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol', 'mergecells', 'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols', 'charts', '|',
+					            'preview', 'searchreplace', 'help'
+					        ]]
 			};
 			var ue = UE.getEditor('container', options);
 		}
