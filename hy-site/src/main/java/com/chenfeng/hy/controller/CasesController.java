a86ecafcfde08c2db284f57b67b7c509400c2ba1
wyhmonstar <XLyh13648000851>
2016-09-27 07:16:47 +0800

    no message

--- hy-site/src/main/java/com/chenfeng/hy/controller/CasesController.java
+++ hy-site/src/main/java/com/chenfeng/hy/controller/CasesController.java
@@ -10,12 +10,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.chenfeng.hy.domain.model.Cases;
 import com.chenfeng.hy.domain.model.CasesDetail;
+import com.chenfeng.hy.domain.model.vo.BsgridVo;
 import com.chenfeng.hy.service.CasesDetailService;
 import com.chenfeng.hy.service.CasesService;
+import com.github.pagehelper.Page;
 
 @Controller
 @RequestMapping("cases")
@@ -34,6 +38,30 @@ public class CasesController {
     	List<Cases> cases = casesService.findAll();
     	model.addAttribute("cases", cases);
         return "cases/index";
+    }
+    
+    @RequestMapping(value = "query", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public BsgridVo<Cases> query(@RequestParam(required = false, defaultValue = "1", value = "curPage") int curPage, 
+            @RequestParam("pageSize") Integer pageSize) {
+    	
+    	BsgridVo<Cases> bsgridVo = null;
+    	try {
+			// 调用service获取数据
+    		Page<Cases> page = casesService.query(curPage, pageSize);
+			bsgridVo = new BsgridVo<Cases>();
+			bsgridVo.setCurPage(Long.parseLong(page.getPageNum()+""));
+			bsgridVo.setData(page.getResult());
+			bsgridVo.setSuccess(true);
+			bsgridVo.setTotalRows(page.getTotal());
+			bsgridVo.setTotalPages(Long.parseLong(page.getPages()+""));
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.error("获取动态数据异常：" + e.getMessage());
+		}
+    	return bsgridVo;
+    	
     }
 
     @RequestMapping(value = "forDetail/{id}", method = RequestMethod.GET)
